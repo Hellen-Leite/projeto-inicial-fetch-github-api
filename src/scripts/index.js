08dfce7ebc28f1b28375d1b74ecbe8e202f08306
Hellen-Leite <hellenleitearaujo16@gmail.com>
2024-08-25 14:29:11 -0300

    edição em formatação de texto/código

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,10 +5,10 @@ import { screen } from "./objects/screen.js"
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
-    if (validateEmptyInput(userName)) return
+    if (validateEmptyInput(userName))
+        return
     getUserData(userName)
 })
-
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
     const userName = e.target.value
@@ -16,26 +16,23 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const isEnterKeyPressed = key === 13
 
     if (isEnterKeyPressed) {
-        if (validateEmptyInput(userName)) return
+        if (validateEmptyInput(userName))
+            return
         getUserData(userName)
     }
 })
-
-
 
 function validateEmptyInput(userName) {
     if (userName.length === 0) {
         alert("Prencha o campo com o nome do usuário do GitHub")
         return true
     }
-
 }
 
 async function getUserData(userName) {
     const userResponse = await getUser(userName)
     if (userResponse.message === "Not Found") {
         screen.renderNotFound()
-
         return
     }
 
@@ -45,11 +42,4 @@ async function getUserData(userName) {
     user.setRepositories(repositoriesResponse)
 
     screen.renderUser(user)
-    // getUser(userName).then(userData => {
-    //     
-    //     getUserRepositories(userName)
 }
-//)
-//}
-
-
