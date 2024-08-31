872107476f38dc8d9d0d45cc235adfb511c54042
Hellen-Leite <hellenleitearaujo16@gmail.com>
2024-08-30 22:01:18 -0300

    adição de novas funções

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,12 +2,14 @@ import { getUser } from "./services/user.js"
 import { getRepos } from "./services/repositories.js"
 import { user } from "./objects/user.js"
 import { screen } from "./objects/screen.js"
+import { getEvents } from "./services/events.js"
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
     if (validateEmptyInput(userName))
         return
     getUserData(userName)
+    getUserEvents(userName)
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
@@ -19,6 +21,7 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
         if (validateEmptyInput(userName))
             return
         getUserData(userName)
+        getUserEvents(userName)
     }
 })
 
@@ -42,4 +45,24 @@ async function getUserData(userName) {
     user.setRepositories(repositoriesResponse)
 
     screen.renderUser(user)
+    getUserEvents(userName)
+
+}
+function getUserEvents(userName) {
+    getEvents(userName).then(eventesData => {
+        let eventsItens = ""
+        eventesData.forEach(item => {
+            if (item.type === "PushEvent") {
+                eventsItens += `<div class="events"><li>${item.repo.name}: <p class="commits"> -${item.payload.commits[0].message}</p></li> </div>`
+            }
+            else { eventsItens += `${item.repo.name}: <div class="events"><p class="strong">Sem mensagem de commit</p></div>` }
+        })
+
+        document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
+        <h2>Últimos Eventos</h2>
+        <div class="events">
+        <ul>${eventsItens}</ul>
+        </div>
+        </div>`
+    })
 }
--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,3 +1,4 @@
+
 const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user) {
@@ -6,11 +7,21 @@ const screen = {
                             <div class="data">
                                  <h1>${user.name ?? 'Não possui nome cadastrado :"('}</h1>
                                  <p>${user.bio ?? 'Não possui bio cadastrada :"('}</p>
+                                 <br>
+                                 <span>${user.followers} seguidores  |</span>
+                                 <span>|  ${user.following} seguindo</span>
                             </div>
                         </div>`
 
         let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`);
+        user.repositories.forEach(repo =>
+            repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            <div class="reposit">
+                <p>Forks:${repo.forks}</p> 
+                <p>Watchers:${repo.watchers}</p>
+                <p>Stars:${repo.stargazers_count}</p>
+                <p>Linguagem utilizada:${repo.language}</p>
+            </div></li>`);
 
         if (user.repositories.length > 0) {
             this.userProfile.innerHTML += `<div class="repositories section">
@@ -18,6 +29,16 @@ const screen = {
                <ul>${repositoriesItens}</ul>
                </div>`
         }
+
+
+
+
+
+
+
+        // let events = ''
+        // getEvents.forEach(eventss => events = `<li style="display: flex">Repositório:${getEvents.repo.name} Commit: ${getEvents.payload.commits.message}</li>`)
+
     },
     renderNotFound() {
         this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
