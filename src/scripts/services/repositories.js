a8924ebb5c0c4e0d00706bcbfc2b0afbf0a0b6fc
Hellen-Leite <hellenleitearaujo16@gmail.com>
2024-09-14 10:49:04 -0300

    mudança no repositories

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,8 +1,11 @@
 import { baseUrl, repositoriesEEventsQuantity } from "../variables.js"
 
 async function getRepos(userName) {
-    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesEEventsQuantity}`)
-    return await response.json()
+    const response = await fetch(`${baseUrl}/${userName}/repos`)
+    const repositories = await response.json()
+    return repositories.slice(0, repositoriesEEventsQuantity)
 }
 
 export { getRepos }
+
+//?per_page=${repositoriesEEventsQuantity}
